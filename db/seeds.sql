4a9aa24bb89aaa522b674b10bc4c04d2f4fb2ef1
The Becs <beccyvalentine@gmail.com>
2019-06-21 11:41:03 -0700

    addition of the db and config files

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Turekey Burger');
+INSERT INTO burgers (burger_name) VALUES ('Ostrich Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Quarter LB Beef', true);
